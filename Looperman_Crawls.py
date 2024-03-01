41a9c7b0ce808044e171795b4419ffb7196295ce
emilyng-sz <emilyng.sz831@gmail.com>
2024-03-01 09:21:58 +0100

    Retrieve tag related information

--- Looperman_Crawls.py
+++ Looperman_Crawls.py
@@ -19,3 +19,19 @@ for div in player_wrappers:
         # the titles will then start with "https://www.looperman.com/media/loops/2273068/looperman-l-2273068-0353864-" "
         song_dictionary[title] = {'download_link': rel_link}
         song_dictionary[title]["full_name"] = full_title
+
+    tag_divs = soup.find_all('div', class_=lambda x: x and x.startswith('tag'))
+    desired_categories = 'Bass/Bass Guitar/Bass Synth/Bass Wobble/Drum/Flute/Guitar Acoustic/Guitar Electric/Harp/Percussion/Piano/Scratch/Strings/Synth/Violin'.split('/')
+    to_remove = [] # store titles that do not belong in the category
+
+for div in tag_divs:
+    # Find all <a> elements within the current <div> that have href containing the title text
+    links = div.find_all('a', href=lambda href: href and href.startswith('https://www.looperman.com/loops/detail/'))
+    assert len(links) == 1, f"multiple titles in {div}"
+
+    ## get same format as downloaded text
+    href = links[0].get('href')  # Get the value of the href attribute
+    title_tuple = href.split('/')[-1].split('-')
+    bpm_index = next((i for i, x in enumerate(title_tuple) if 'bpm' in x), None)
+    title_tuple = title_tuple[:bpm_index-1]
+    title = '-'.join(title_tuple) # this assumes the title is followed by 'free', 'bpm' etc 
